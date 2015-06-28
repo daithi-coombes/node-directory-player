805f2e3cb46677e1f2f86e1571946b0346c70c3f
Daithi Coombes <webeire@gmail.com>
2015-06-28 13:46:22 +0100

    add player as dependency
    Signed-off-by: Daithi Coombes <webeire@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var Player = require('player'),
-    __dirname = '/media/daithi/DATA/Music/thumpin/'
+    __dirname = '/media/daithi/DATA/Music/psyanide/'
 
 var player = new Player([
-    __dirname + 'Radikal Guru & Cian Finn - Ireland Live @ Czworka.mp3',
-    __dirname + 'KAMUI-ELECTRO SLUT(ASYS REMIX).mp3',
-    __dirname + 'BK & Vinyl Groover - Insomniak [HQ] (Request).mp3',
-    __dirname + 'BK-Dirtbox.mp3',
-    __dirname + '[DANCE] 2 many DJ\'s, Funky House!!! _DIRTY.mp3',
-    __dirname + 'Ben Sims - Remanipulated.mp3'
+    __dirname + '10 Lyrix - afrika soul.mp3',
+    __dirname + '06 Module - snake skin radio.mp3',
+    __dirname + '04 Smurf - mindless.mp3'
 ])
 
 player.play()
